7b7e8da55c7bd388f573d506bb5cd6774bdbfa6d
xiaoniu2014 <116445168@qq.com>
2015-06-30 13:10:42 +0800

    Initial Commit

---
+++ HWTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWTabBar
+//
+//  Created by hw on 15/6/30.
+//  Copyright (c) 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HWTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HWTabBar
+//
+//  Created by hw on 15/6/30.
+//  Copyright (c) 2015年 hongw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
